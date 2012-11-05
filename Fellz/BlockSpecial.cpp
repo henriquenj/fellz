964ba5dee7ba9e774e150ffc3f62c1016b319ea4
henriquenj@gmail.com <henriquenj@gmail.com@8a670fb0-f419-63f6-d3cc-0ff17b183091>
2012-11-05 19:13:16 +0000

    * More on powerups

--- Fellz/BlockSpecial.cpp
+++ Fellz/BlockSpecial.cpp
@@ -8,6 +8,7 @@ BlockSpecial::BlockSpecial(int kind) //: PowerUpBase()
 	// put sprite on power up zone
 	CCSprite* sprite = CCSprite::create("Assets/redSpecial.png");
 	sprite->setPosition(ccp(730.0f,520.0f));
+	
 	this->addChild(sprite);
 }
 
@@ -25,5 +26,34 @@ BlockSpecial* BlockSpecial::create(int kind)
 
 void BlockSpecial::Execute()
 {
-		
+	std::list<ColorBlock*> *blocksList = ((MainGameScene*)this->getParent())->GetBlockList();
+
+	// schedule the deletation with a little bit of difference in time
+	float time = 0.2;
+
+	//iterate through the list
+	std::list<ColorBlock*>::iterator it;
+	for (it = (*blocksList).begin();it != (*blocksList).end(); it++)
+	{
+		// if the block is the sabe kind of the special, anihilated
+		if ((*it)->GetBlockColor() == kind)
+		{
+			CCSequence* sequence = CCSequence::create(CCDelayTime::create(time),CCCallFuncO::create(this,callfuncO_selector(BlockSpecial::DeleteBlockCallback),*it));
+			this->runAction(sequence);
+			time += 0.1;
+
+		}
+	}
+}
+
+
+void BlockSpecial::DeleteBlockCallback(CCObject* block)
+{
+	if (!((ColorBlock*)block)->GetDying())
+	{
+		// draw points
+		pointsManager->AnihilationHappened(2,((ColorBlock*)block)->getPosition());
+		// destroy block
+		((ColorBlock*)block)->Destroy();
+	}
 }
