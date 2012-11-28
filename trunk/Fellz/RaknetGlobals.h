8d504a71beed2b6fafa1dd89057fb1d46318edfd
henriquenj@gmail.com <henriquenj@gmail.com@8a670fb0-f419-63f6-d3cc-0ff17b183091>
2012-11-28 13:07:48 +0000

    * Init raknet stuff

---
+++ trunk/Fellz/RaknetGlobals.h
@@ -0,0 +1,15 @@
+// Raknet globals holds the global objects and related defines
+
+
+#ifndef __RAKNET_GLOBALS__
+#define __RAKNET_GLOBALS__
+
+#include "RakPeerInterface.h"
+
+#define SERVER_PORT 5999
+
+// main interface
+extern RakNet::RakPeerInterface* player2;
+extern bool isServer;
+
+#endif // __RAKNET_GLOBALS__
